cd79c25dc7cd77a0c85787ab22933d5049d36205
Sean Logan Para <spara77@gmail.com>
2018-04-26 14:38:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function yourAnimal() { var animal = 'cat'
 }
 
 function add2(n) {
-  return n + 2
+   const two = 2 return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
